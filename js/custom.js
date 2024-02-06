eaa5170de149a7d64a9915feeccb08c0a192ea5a
서우승 <148739245+SeoWooSeung@users.noreply.github.com>
2024-02-06 18:12:47 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,97 +1,88 @@
 $(function () {
+  $(".main_slide").slick({
+    arrows: false,
+    autoplay: true,
+    pauseOnHover: false,
+    dots: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    fade: true,
+  });
 
-    $('.main_slide').slick({
-        arrows: false,
-        autoplay: true,
-        pauseOnHover: false,
-        dots: false,
-        speed:1500,
-        autoplaySpeed:4000,
-        fade: true,
-    });
-    
+  $(".main_slide").on("init afterChange", function (e, s, c) {
+    const current = $("main_slide .slilck-current");
 
-    $('.main_slide').on('init afterChange', function (e, s, c) {
-        const current = $('main_slide .slilck-current');
+    console.log(c, s.slideCount);
 
-        console.log(c, s.slideCount);
+    $(".num span").text(s.slideCount);
+    $(".num strong").text(c ? c + 1 : 1);
 
-        $('.num span').text(s.slideCount);
-        $('.num strong').text(c ? (c + 1) : 1);
+    current.addClass("on").siblings().removeClass("on");
 
-        current
-            .addClass('on')
-            .siblings()
-            .removeClass('on')
+    $(".visual_slide .menu li").eq(0).addClass("on");
 
-        $('.visual_slide .menu li')
-            .eq(0)
-            .addClass('on');
+    $(".visual_slide .menu li")
+      .eq(c)
+      .addClass("on")
+      .siblings()
+      .removeClass("on");
+  });
 
-        $('.visual_slide .menu li')
-            .eq(c)
-            .addClass('on')
-            .siblings()
-            .removeClass('on');
+  $(".sub_slide").slick({
+    arrows: false,
+    autoplay: false,
+    pauseOnHover: false,
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    speed: 1000,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 
-    });
-    $('.sub_slide').slick({
-        arrows: false,
-        autoplay: false,
-        pauseOnHover: false,
-        dots: false,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        speed:1500,
-        responsive:[
-            {
-                breakpoint: 1200,
-                settings : {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                }
-            } ,
-            {
-                breakpoint: 769,
-                settings : {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            }            
-        ],
-    });
+  $(".visual_slide .arrows .left").on("click", function () {
+    $(".main_slide").slick("slickPrev");
+  });
 
+  $(".visual_slide .arrows .right").on("click", function () {
+    $(".main_slide").slick("slickNext");
+  });
 
-    $('.visual_slide .arrows .left').on('click', function () {
-        $('.main_slide').slick('slickPrev')
-    });
+  $(".visual_sub_slide .arrows .left").on("click", function () {
+    $(".sub_slide").slick("slickPrev");
+  });
 
-    $('.visual_slide .arrows .right').on('click', function () {
-        $('.main_slide').slick('slickNext')
-    });
+  $(".visual_sub_slide .arrows .right").on("click", function () {
+    $(".sub_slide").slick("slickNext");
+  });
 
+  $(".main_visual .main_slide").on("afterChange", function (e, s, c) {
+    console.log(e, s, c); //c=0,1
+    $(".main_visual .num span").text("0" + (c + 1));
+  });
 
-    $('.visual_sub_slide .arrows .left').on('click', function () {
-        $('.sub_slide').slick('slickPrev')
-    });
+  $(".mbt").on("click", function () {
+    $(".gnb").toggleClass("on");
+    $(".h1").toggleClass("on");
+    $(".header").toggleClass("on");
+    $(".icon").toggleClass("on");
+  });
 
-    $('.visual_sub_slide .arrows .right').on('click', function () {
-        $('.sub_slide').slick('slickNext')
-    });
-
-    $('.main_visual .main_slide').on('afterChange', function (e, s, c) {
-        console.log(e, s, c);//c=0,1
-        $('.main_visual .num span').text('0' + (c + 1));
-    })
-
-    
-    $('.mbt').on("click", function () {
-        $('.gnb').toggleClass('on');
-        $('.h1').toggleClass('on');
-        $('.header').toggleClass('on');
-        $('.icon').toggleClass('on');
-    })
-
-
-    
+  $(".mbtn").on("click", () => {
+    $("#header").toggleClass("active");
+  });
 });
